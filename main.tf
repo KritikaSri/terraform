9d069f0221f19fd2aad7d706edcd82832e38cefe
choppyfish46 <choppyfish46@gmail.com>
2019-05-02 17:19:48 -0700

    made changes

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-2"
 }
 
 
-resource "aws_instance" "example" {
-  ami                    = "ami-01ed306a12b7d1c96)"
-  instance_type          = "t2.micro"
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "test-vpc"
+  }
 }
