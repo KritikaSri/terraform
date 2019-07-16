e5bc86a7890183aceebdb74bf1756d90724ff7fc
choppyfish46 <choppyfish46@gmail.com>
2019-07-16 16:38:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,37 @@ provider "aws" {
 
 
 
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
 
-  tags {
-    Name = "test-vpc"
+module "vpc" {
+  source = "../../"
+
+  name = "simple-example"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  assign_generated_ipv6_cidr_block = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
   }
 }
